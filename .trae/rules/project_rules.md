37d0c17911569c53730b3acb1cc7ec1ef6685bd2
arc/welki/FlameC <91359913+FlameChart@users.noreply.github.com>
2025-05-17 14:36:23 +0800

    Initial Commit

---
+++ .trae/rules/project_rules.md
@@ -0,0 +1,5 @@
+reply as short as possible. only include the information that is relevant to the issue. talk is cheap, show me the code only. use as many cli command as possible. using cmd/powershell because we are currently on windows.
+
+we are using tauri + react + typescript + tailwindcss + vite now. make sure you are naming the variables by their meanings and not just using the names of the components.
+
+make the page/function as modulized as possible.
---
+++ src-tauri/src/lib.rs
@@ -0,0 +1,25 @@
+use tauri::{AppHandle, Manager};
+
+// Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
+#[tauri::command]
+fn greet(name: &str) -> String {
+    format!("Hello, {}! You've been greeted from Rust!", name)
+}
+
+#[cfg_attr(mobile, tauri::mobile_entry_point)]
+pub fn run() {
+    tauri::Builder::default()
+        .plugin(tauri_plugin_notification::init())
+        .plugin(tauri_plugin_window_state::Builder::new().build())
+        .plugin(tauri_plugin_single_instance::init(|app, args, cwd| {
+            let _ = app
+                .get_webview_window("main")
+                .expect("no main window")
+                .set_focus();
+        }))
+        .plugin(tauri_plugin_clipboard_manager::init())
+        .plugin(tauri_plugin_opener::init())
+        .invoke_handler(tauri::generate_handler![greet])
+        .run(tauri::generate_context!())
+        .expect("error while running tauri application");
+}
